6a70486689a998f295c3c28141fb36a9bf700a3e
unknown <sanskargajbhiye0@gmail.com>
2025-01-07 23:06:04 +0530

    minor changes

--- modules, comment and pip/C1-PS/problem2edit.py
+++ modules, comment and pip/C1-PS/problem2edit.py
@@ -1,11 +1,11 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-gender = input("Are you male or female? (male/female): ").strip().lower()
+gender = input("Are you male or female? (m/f): ").strip().lower()
 
-if gender == "male":
+if gender == "m":
     engine.say("What can I do for you, sir?")
-elif gender == "female":
+elif gender == "f":
     engine.say("What can I do for you, madam?")
 else:
     engine.say("What can I do for you?")
