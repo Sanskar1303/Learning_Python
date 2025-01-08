e89a84aacdf15dc505508d6a08bab7c850808bd7
unknown <sanskargajbhiye0@gmail.com>
2025-01-09 00:38:05 +0530

    Adding my Progress

---
+++ Strings/que2.py
@@ -0,0 +1,5 @@
+letter = '''
+            Dear <|Name|>
+            You are selected
+            <|Date|>'''
+print(letter.replace("<|Name|>","Sanskar Gajbhiye").replace("<|Date|>","07 Jan"))
---
+++ Strings/que3.py
@@ -0,0 +1,7 @@
+name = ("What the fuck  is  happning")
+
+print(name.find("  "))
+
+print(name.replace("  "," ").replace("fuck","duck"))
+
+print(name) #strings are immutable
